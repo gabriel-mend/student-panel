2f2945cafa045881da1c898e5a64adf24f3a761f
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-09-24 14:33:49 -0300

    adding fonts in head and finish theme.ts

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "../styles/global"
-import theme from "../styles/theme"
-
+import { GlobalStyle } from "styles/global"
+import theme from "styles/theme"
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return <ThemeProvider theme={theme}>
+    <Head>
+      <title>Student Panel</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaina+2:wght@400;600&family=Roboto&display=swap" rel="stylesheet" />
+    </Head>
     <GlobalStyle />
     <Component {...pageProps} />
   </ThemeProvider>
--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -3,18 +3,25 @@ export default {
     container: '1440px'
   },
   font: {
-    family: "",
+    family: {
+      balooBhaina2: 'Baloo Bhaina 2, cursive',
+      roboto: 'Roboto, sans-serif'
+    },
     weight: {
-
+      regular: 400
     },
     sizes: {
-
+      small: '12px',
+      medium: '14px',
+      large: '16px',
+      xlarge: '18px',
+      xxlarge: '24px',
+      huge: '36px'
     }
   },
   colors: {
-
-  },
-  spacings: {
-
+    primary: '#000000',
+    secondary: '#ffffff',
+    tertiary: '#F5F5F7'
   },
 }
